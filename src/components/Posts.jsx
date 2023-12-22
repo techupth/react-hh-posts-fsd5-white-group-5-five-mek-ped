9fc66f46f3fefca13c936071b6459f5f0c6ed6fd
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-12-22 13:45:48 +0700

    feat:complete Ex

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,29 +1,53 @@
+import { useState } from "react";
+import postList from "./data/post-list";
 function Posts() {
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+      {postList.map((item, index) => {
+        const [like, setLike] = useState(item.likes);
+        const handleLike = () => {
+          setLike(like + 1);
+        };
+        const handleDisLike = () => {
+          if (like != 0) {
+            setLike(like - 1);
+          }
+        };
+
+        return (
+          <div class="post-list" key={index}>
+            <div
+              class="post-item"
+              style={{
+                width: "900px",
+              }}
+            >
+              <div class="post-header">
+                <h2>{`${item.title} #${item.id}`}</h2>
+                <div class="post-social-media-stats">
+                  <span class="stats-topic">Likes: </span>
+                  <span class="post-likes">{like}</span>
+                </div>
+              </div>
+              <p class="post-content">{item.content}</p>
+              <div class="post-actions">
+                <button
+                  class="like-button"
+                  onClick={() => {
+                    handleLike(item.likes);
+                  }}
+                >
+                  Like
+                </button>
+                <button class="dislike-button" onClick={handleDisLike}>
+                  Dislike
+                </button>
+              </div>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
